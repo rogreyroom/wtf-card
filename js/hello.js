cdb2168a377a89b7c74a80a88ddffa770d8f789f
Robert Adamczewski <robert.greyroom@gmail.com>
2019-06-20 08:43:18 +0200

    improvment: improve data input handler

--- js/hello.js
+++ js/hello.js
@@ -1,18 +1,55 @@
 const great = document.querySelector(".hello--js");
 const hi = document.querySelector(".hi--js");
 
-const hello = (name, age) => {
-  return `Miło Cię poznać.<br>
-    ${name}. O! masz ${age} lat.<br>
-    ${age > 31 ? "Ja mam nieco mniej." : "Ja mam nieco więcej."}`;
-};
+const hello = (name, age) => `Miło Cię poznać.<br>
+                             ${name}. O! Twój wiek to ${age}.<br>
+                             ${
+                               age > 31
+                                 ? "Mój jest nieco mniejszy."
+                                 : "Mój jest nieco większy."
+                             }`;
+
+const helloName = name => `Miło Cię poznać! ${name}`;
+
+const helloAge = age => `Miło Cię poznać nieznajomy! <br>
+                         Więc Twój wiek to ${age}. <br>
+                         ${
+                           age > 31
+                             ? "Mój jest nieco mniejszy."
+                             : "Mój jest nieco większy."
+                         }`;
 
 const ask = () => {
+  let name, nameValue;
+  let age, ageValue;
   name = prompt("Jak masz na imię?");
-  age = prompt("Ile masz lat?");
+  age = Number(prompt("Ile masz lat?"));
 
-  great.innerHTML = hello(name, age);
-  great.classList.add("hello--active");
+  if (name === null || name.trim() === "") {
+    nameValue = false;
+  } else {
+    nameValue = name.charAt(0).toUpperCase() + name.slice(1);
+  }
+
+  if (age === 0 || isNaN(age)) {
+    ageValue = false;
+  } else {
+    ageValue = age;
+  }
+
+  if (nameValue !== false && ageValue !== false) {
+    great.innerHTML = hello(nameValue, ageValue);
+    great.classList.add("hello--active");
+  } else if (nameValue !== false && ageValue === false) {
+    great.innerHTML = helloName(nameValue);
+    great.classList.add("hello--active");
+  } else if (nameValue === false && ageValue !== false) {
+    great.innerHTML = helloAge(ageValue);
+    great.classList.add("hello--active");
+  } else {
+    great.innerHTML = `Witaj nieznajomy.`;
+    great.classList.add("hello--active");
+  }
 };
 
 const greatOff = () => {
